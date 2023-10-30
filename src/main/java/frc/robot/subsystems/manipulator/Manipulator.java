24c906b8e89255799df24e49fae2984fab5fb610
Jacob M <jcmertz@outlook.com>
2023-10-30 16:47:16 -0400

    working on Manpulator

--- src/main/java/frc/robot/subsystems/manipulator/Manipulator.java
+++ src/main/java/frc/robot/subsystems/manipulator/Manipulator.java
@@ -1,9 +1,23 @@
 package frc.robot.subsystems.manipulator;
 
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Manipulator extends SubsystemBase{
     
+    private final ManipulatorIO io;
+    private final ManipulatorIOInputsAutoLogged inputs = new ManipulatorIOInputsAutoLogged();
     
+    public Manipulator(ManipulatorIO io) {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Manipulator", inputs);
+    }
 
 }
--- src/main/java/frc/robot/subsystems/manipulator/ManipulatorIO.java
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorIO.java
@@ -1,7 +1,14 @@
 package frc.robot.subsystems.manipulator;
 
+import org.littletonrobotics.junction.AutoLog;
+
 public interface ManipulatorIO {
 
-    
+    @AutoLog
+    public static class ManipulatorIOInputs {
+        
+    }
+
+    public default void updateInputs(ManipulatorIOInputs inputs) {}
     
 }
