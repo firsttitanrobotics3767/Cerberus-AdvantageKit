cde767ccbab41f0f8609ee48953afa6c2a35f627
Jacksonhoch1 <jacksonhoch1@gmail.com>
2023-11-09 11:57:58 -0500

    working on arm logging (IO close to complete)

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -5,38 +5,26 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ArmIO {
     @AutoLog
     public static class ArmIOInputs {
-        ////////////////////////////////////////////////
-        //pivot
-        public double rotationVositionRads = 0.0;
-        public double rotationVelocityRadsPerSec = 0.0;
-        public double rotationVolts = 0.0;
-        public double rotationAmps = 0.0;
-        public boolean upperLimitSwitchState = false;
-        public boolean lowerLimitSwitchState = false;
-        /////////////////////////////////////////
-        //extention
-        public double extentionVositionInches = 0.0;
-        public double extentionVelocityInchesPerSec = 0.0;
-        public double extentionVolts = 0.0;
-        public double extentionAmps = 0.0;
+        public double positionInches = 0.0;
+        public double velocityInchesPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
         public boolean forwardLimitSwitchState = false;
         public boolean reverseLimitSwitchState = false;
     }
-    //pivot
+
     public default void updateInputs(ArmIOInputs inputs) {}
 
-    public default void setRotationVolts(double volts) {}
+    public default void setVolts(double volts) {}
 
-    public default void setRotationposition(double volasetypositoinRads) {}
+    public default void setPosition(double positionInches) {}
 
-    public default void stopRotation() {}
+    public default void stop() {}
 
-    public default void configureRotationPID() {}
+    public default void configurePID(double p, double i, double d) {}
 
-    public default void configureRotatoinPidConstrants(double maxVolosetyRadsPerSec, double maxExelerationRadsPersecPerSec) {} 
+    public default void configureRotatoinPidConstraints(double maxVolosetyRadsPerSec, double maxExelerationRadsPersecPerSec) {} 
 
     public default void configureRotationalLimits(double upperLimit, double LowerLimit) {}
-    // extention
-
 
 }
