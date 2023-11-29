832d5e364d10d5062a9ed2c9f41fd10fffc728c6
Jacksonhoch1 <jacksonhoch1@gmail.com>
2023-11-28 22:27:28 -0500

    idek everything is fubar

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.arm;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Arm extends SubsystemBase {
+public class Arm extends SubsystemBase { 
     
     
 
