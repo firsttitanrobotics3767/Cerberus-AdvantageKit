6e9969776f045cd2d8b6a45c3b5993a9d4520010
Jacksonhoch1 <jacksonhoch1@gmail.com>
2023-11-28 21:41:28 -0500

    reverted to 106376e

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems.arm;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Arm extends SubsystemBase {
-    public final ArmIO
-    
-
-}
--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.arm;
-
-import com.revrobotics.CANSparkMax;
-
-public class ArmIOReal implements ArmIO {
-    private CANSparkMax armMotor
-}
